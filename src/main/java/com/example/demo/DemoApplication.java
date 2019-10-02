f2af8347a6bcc9d3c9a6e0582ba7a90ec4bdf622
Aswin-Venkat <aswinven@gmail.com>
2019-10-02 17:11:25 +0100

    application build test 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World test!";
+		return "Hello World test with jenkins!";
 	}
 
 	public static void main(String[] args) {
